311815c31cbdebc836ae572d03b2a930a6453ff1
FelicityMJ <91504041+FelicityMJ@users.noreply.github.com>
2026-08-17 18:21:34 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,10 @@
 export const firebaseEnabled = true;
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyB4kbf48KHI9h7nA79miWDlFjIma7ZNr_o",
-  authDomain: "dataapp-studio.firebaseapp.com",
-  projectId: "dataapp-studio",
-  storageBucket: "dataapp-studio.firebasestorage.app",
-  messagingSenderId: "789599432969",
-  appId: "1:789599432969:web:f789b713567a652cca63d8"
+  apiKey: "AIzaSyA68yWP1G2pkPNTSz_J2NDR1SW_nP1krUg",
+  authDomain: "app-design-studio-42d74.firebaseapp.com",
+  projectId: "app-design-studio-42d74",
+  storageBucket: "app-design-studio-42d74.firebasestorage.app",
+  messagingSenderId: "310906546595",
+  appId: "1:310906546595:web:2d89f9f93233e1c0eed216"
 };
