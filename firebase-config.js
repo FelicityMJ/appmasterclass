27c9a883440508a42ca518c5dfb077b6f1ba4c10
FelicityMJ <91504041+FelicityMJ@users.noreply.github.com>
2026-08-17 18:03:28 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Safe default: the classroom prototype runs locally until you add your Firebase Web App config.
-export const firebaseEnabled = false;
+export const firebaseEnabled = true;
 
 export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyB4kbf48KHI9h7nA79miWDlFjIma7ZNr_o",
+  authDomain: "dataapp-studio.firebaseapp.com",
+  projectId: "dataapp-studio",
+  storageBucket: "dataapp-studio.firebasestorage.app",
+  messagingSenderId: "789599432969",
+  appId: "1:789599432969:web:f789b713567a652cca63d8"
 };
